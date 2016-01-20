87228995f5594beb48c3f0e225943b6cd87c07ec
ElstJ-AP <jonas.elst@hotmail.com>
2016-01-20 15:40:13 +0100

    Initial setup
    Created couchapp project, copied jquery.js and created main.js. Also
    modified index.html

---
+++ app/_attachments/scripts/main.js
@@ -0,0 +1,13 @@
+
+
+var DB = "http://127.0.0.1:5984/todo/";
+//var VIEW = "_design/app/_view/todoSearch?key=%22";
+
+function searchTodo(){
+	
+	$('#content').empty();
+    
+	var todo = $('#todo').val();
+	
+
+}
